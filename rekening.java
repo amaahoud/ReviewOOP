d576e73749c7e5f1ce232098ed52a490be80c6d2
amaahoud <sitirahmaalia3@gmail.com>
2025-03-14 19:11:24 +0700

    Update rekening.java

--- rekening.java
+++ rekening.java
@@ -1,33 +1,59 @@
-public class rekening {
-    String NamaPemilik ;
-    String NomorRekening;
-    double Saldo;
-
-    public rekening(String NamaPeilik, String NomorRekening, double Saldo ) {
-        this.NamaPemilik = NamaPemilik;
-        this.NomorRekening = NomorRekening;
-        this.Saldo = Saldo;
-    }
-
-    public void Setor(double jumlah){
-        Saldo += jumlah;
-    }
-
-    public void Tarik(double jumlah){
-        if (jumlah <= Saldo){
-            Saldo -= jumlah;
-        } else{
-            System.out.println("Saldo tidak Cukup");
-        }
-
-        
-    }
-
-    public void tampilkanInfo(){
-        System.out.println("Nama: " + NamaPemilik);
-        System.out.println("Nomor Rekening: " + NomorRekening);
-        System.out.println("jumlah Saldo: " + Saldo);
-        System.out.println("==========================");
+public class Rekening {
+    String namaPemilik;
+    String nomorRekening;
+    double saldo;
+    
+    public Rekening(String namaPemilik, String nomorRekening, double saldo) {
+        this.namaPemilik = namaPemilik;
+        this.nomorRekening = nomorRekening;
+        this.saldo = saldo;
     }
     
+    public String getNamaPemilik() {
+        return namaPemilik;
+    }
+    
+    public void setNamaPemilik(String namaPemilik) {
+        this.namaPemilik = namaPemilik;
+    }
+    
+    public String getNomorRekening() {
+        return nomorRekening;
+    }
+    
+    public void setNomorRekening(String nomorRekening) {
+        this.nomorRekening = nomorRekening;
+    }
+    
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public void setor(double jumlah) {
+        if (jumlah > 0) {
+            saldo += jumlah;
+            System.out.println("Setor: " + jumlah);
+        } else {
+            System.out.println("Jumlah setor tidak valid.");
+        }
+    }
+    
+    public void tarik(double jumlah) {
+        if (jumlah > saldo) {
+            System.out.println("Saldo tidak cukup untuk penarikan.");
+        } else {
+            saldo -= jumlah;
+            System.out.println("Tarik: " + jumlah);
+        }
+    }
+    
+    public void tampilkanInfo() {
+        System.out.println("Nama Pemilik: " + namaPemilik);
+        System.out.println("Nomor Rekening: " + nomorRekening);
+        System.out.println("Saldo: " + saldo);
+    }
 }
